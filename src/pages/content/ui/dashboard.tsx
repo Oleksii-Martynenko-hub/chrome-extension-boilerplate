ff477faef24a62390bbddc28ef0c148a87fbf100
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2024-02-03 14:04:33 +0200

    feat: use popup storage to control visibility of dashboard content

--- src/pages/content/ui/dashboard.tsx
+++ src/pages/content/ui/dashboard.tsx
@@ -3,6 +3,7 @@ import { Button, Box } from '@chakra-ui/react';
 
 import useStorage from '@root/src/shared/hooks/useStorage';
 import exampleThemeStorage from '@root/src/shared/storages/exampleThemeStorage';
+import popupStorage from '@root/src/shared/storages/popupStorage';
 
 enum MessageType {
   TOGGLE_POPUP = 'toggle-popup',
@@ -15,21 +16,32 @@ type Message<T = object> = {
 
 export default function Dashboard() {
   const theme = useStorage(exampleThemeStorage);
+  const { isOpen, top, left } = useStorage(popupStorage);
 
   useEffect(() => {
     chrome.runtime.onMessage.addListener((message: Message) => {
       if (message.type === MessageType.TOGGLE_POPUP) {
-        console.log('🚀 ~ chrome.runtime.onMessage.addListener ~ message.type:', message.type);
+        popupStorage.toggle();
       }
     });
   }, []);
 
   return (
-    <Box position={'fixed'} top={0} left={0}>
-      content view with {theme === 'light' ? 'light' : 'dark'}
-      <Button colorScheme={theme === 'light' ? 'teal' : 'purple'} onClick={exampleThemeStorage.toggle}>
-        Toggle theme
-      </Button>
+    <Box position={'fixed'} top={top} left={left}>
+      {isOpen && (
+        <Box bg={theme === 'light' ? 'whitesmoke' : '#222'} color={theme === 'light' ? '#222' : 'white'}>
+          <Button colorScheme={theme === 'light' ? 'teal' : 'purple'} onClick={popupStorage.toggle}>
+            X
+          </Button>
+
+          <Box className="container" p={4}>
+            content view with {theme === 'light' ? 'light' : 'dark'}
+            <Button colorScheme={theme === 'light' ? 'teal' : 'purple'} onClick={exampleThemeStorage.toggle}>
+              Toggle theme
+            </Button>
+          </Box>
+        </Box>
+      )}
     </Box>
   );
 }
